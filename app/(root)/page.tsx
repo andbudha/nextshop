7d082d81527aca5e4565c3fba3a4ea3171591a53
andbudha <andreibartov@gmail.com>
2025-05-25 15:15:01 +0200

    NotFound page created

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,12 @@
 const Home = () => {
-  return <div>NextShop</div>;
+  return (
+    <div>
+      <div className="wrapper flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold">Welcome to Our Store</h1>
+        <p className="mt-4 text-lg">Explore our products and enjoy shopping!</p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ app/not-found.tsx
@@ -0,0 +1,32 @@
+'use client';
+import logo from '../public/images/logo.png';
+import { Button } from '@/components/ui/button';
+import { APP_NAME } from '@/lib/constants';
+import Image from 'next/image';
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="flex flex-col items-center justify-center p-6 w-1/3 rounded-lg shadow-md text-center">
+        <Image
+          src={logo}
+          height={50}
+          width={50}
+          alt={`${APP_NAME} logo`}
+          priority={true}
+        />
+        <h1 className="text-3xl font-bold mt-4 mb-2">Not Found</h1>
+        <p className="text-destructive mb-4">Could not find requested page</p>
+        <Button
+          className=" flex items-center justify-center"
+          variant={'outline'}
+          onClick={() => (window.location.href = '/')}
+        >
+          Back To Home
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
