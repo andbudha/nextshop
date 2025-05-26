f36222e08af2b6683391fceb80b4d378c48a7288
andbudha <andreibartov@gmail.com>
2025-05-26 10:17:50 +0200

    sampleData added/product-list comp created

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,17 @@
+import ProductList from '@/components/shared/product/product-list';
+import sampleData from '@/db/sample-data';
+
 const Home = () => {
+  console.log('sampleData', sampleData);
+
   return (
-    <div>
-      <div className="wrapper flex flex-col items-center justify-center h-screen">
-        <h1 className="text-4xl font-bold">Welcome to Our Store</h1>
-        <p className="mt-4 text-lg">Explore our products and enjoy shopping!</p>
-      </div>
-    </div>
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={6}
+      />
+    </>
   );
 };
 
