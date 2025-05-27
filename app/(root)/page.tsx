983d565c2c8c9c138e55dc4283e831b3e850512c
andbudha <andreibartov@gmail.com>
2025-05-27 11:20:20 +0200

    products getting server action added/products from db fetched/products limit const. added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,15 @@
 import ProductList from '@/components/shared/product/product-list';
-import sampleData from '@/db/my-data';
+import { getLatestProducts } from '@/lib/actions/product.actions';
+import { LATEST_PRODUCTS_LIMIT } from '@/lib/constants';
 
-const Home = () => {
+const Home = async () => {
+  const latestProducts = await getLatestProducts();
   return (
     <>
       <ProductList
-        data={sampleData.products}
+        data={latestProducts}
         title="Newest Arrivals"
-        limit={8}
+        limit={LATEST_PRODUCTS_LIMIT}
       />
     </>
   );
