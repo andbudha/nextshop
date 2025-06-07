26ff7047b6f217a6f98eb7c0e8f523ad12350975
andbudha <andreibartov@gmail.com>
2025-06-07 18:47:21 +0200

    Cartage comp updated

--- app/(root)/cart/cart-table.tsx
+++ app/(root)/cart/cart-table.tsx
@@ -6,12 +6,60 @@ import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart.actions';
 import { Loader, Minus, Plus, ArrowRight } from 'lucide-react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { Cart } from '@/types';
+import { ToastAction } from '@/components/ui/toast';
+import { Cart, CartItem } from '@/types';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { Button } from '@/components/ui/button';
 
 const CartTable = ({ cart }: { cart?: Cart }) => {
   const router = useRouter();
   const { toast } = useToast();
   const [isPending, startTransition] = useTransition();
+
+  const addItemToCartHandler = async (item: CartItem) => {
+    startTransition(async () => {
+      const res = await addItemToCart(item);
+      // If the response is not successful, show an error toast
+      if (!res.success) {
+        toast({
+          variant: 'destructive',
+          description: res.message,
+        });
+        return;
+      }
+      //handle success
+      toast({
+        description: res.message,
+        action: (
+          <ToastAction
+            className="bg-primary text-white hover:bg-gray-800"
+            altText="Go To Cart"
+            onClick={() => router.push('/cart')}
+          >
+            Go To Cart
+          </ToastAction>
+        ),
+      });
+    });
+  };
+  //remove item from cart handler
+  const removeItemFromCartHandler = async (itemId: string) => {
+    startTransition(async () => {
+      const res = await removeItemFromCart(itemId);
+      toast({
+        variant: res.success ? 'default' : 'destructive',
+        description: res.message,
+      });
+      return;
+    });
+  };
 
   return (
     <>
@@ -22,7 +70,72 @@ const CartTable = ({ cart }: { cart?: Cart }) => {
         </div>
       ) : (
         <div className="grid md:grid-cols-4 md:gap-5">
-          <div className="overflow-x-auto md:col-span-3">Table</div>
+          <div className="overflow-x-auto md:col-span-3">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Product</TableHead>
+                  <TableHead className="text-center">Qauntity</TableHead>
+                  <TableHead className="text-right">Price</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {cart.items.map((item) => (
+                  <TableRow key={item.slug}>
+                    <TableCell>
+                      <Link
+                        href={`/product/${item.slug}`}
+                        className="flex items-center"
+                      >
+                        <Image
+                          src={item.image}
+                          alt={item.name}
+                          width={60}
+                          height={60}
+                          className="object-contain"
+                        />
+                        <span className="px-2">{item.name}</span>
+                      </Link>
+                    </TableCell>
+                    <TableCell className="flex-center  gap-2 mt-4">
+                      <Button
+                        disabled={isPending}
+                        type="button"
+                        variant={'outline'}
+                        onClick={() =>
+                          removeItemFromCartHandler(item.productId)
+                        }
+                      >
+                        {isPending ? (
+                          <Loader className="w-4 h-4 animate-spin" />
+                        ) : (
+                          <Minus className="w-4 h-4" />
+                        )}
+                      </Button>
+                      <span className="px-2">{item.quantity}</span>
+                      <Button
+                        disabled={isPending}
+                        type="button"
+                        variant={'outline'}
+                        onClick={() => addItemToCartHandler(item)}
+                      >
+                        {isPending ? (
+                          <Loader className="w-4 h-4 animate-spin" />
+                        ) : (
+                          <Plus className="w-4 h-4" />
+                        )}
+                      </Button>
+                    </TableCell>
+                    <TableCell className="text-right">
+                      <span className="text-lg font-semibold">
+                        ${Number(item.price) * item.quantity}
+                      </span>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
         </div>
       )}
     </>
