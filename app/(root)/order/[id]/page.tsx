3376d0c43109cf1c54016abf6d8e26f0fb1f3aab
andbudha <andreibartov@gmail.com>
2025-06-19 14:25:00 +0200

    both formatId() and formatDateTime() util functions added

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -2,21 +2,22 @@ import { getOrderById } from '@/lib/actions/order.actions';
 import { notFound } from 'next/navigation';
 
 import { Metadata } from 'next';
+import { formatId } from '@/lib/utils';
 
 export const metadata: Metadata = {
   title: 'Order Details',
 };
 const OrderPage = async (props: { params: Promise<{ id: string }> }) => {
   const { id } = await props.params;
+  console.log('ID:::', formatId(id));
 
   const order = await getOrderById(id);
   if (!order) notFound();
-  console.log('ORDER:::', order);
 
   return (
     <div>
       <h1>Order Page</h1>
-      <p>{order.totalPrice.toString()}</p>
+      <p>{order.totalPrice}</p>
     </div>
   );
 };
