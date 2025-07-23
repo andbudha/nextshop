71b0cf08684d4e6af9b07b88cc554317c899820f
andbudha <andreibartov@gmail.com>
2025-07-23 14:19:53 +0200

    OrdersPage created

--- app/user/orders/page.tsx
+++ app/user/orders/page.tsx
@@ -1,7 +1,83 @@
 import { ORDERS_PER_PAGE } from '@/lib/constants';
+import { Metadata } from 'next';
+import { getMyOrders } from '@/lib/actions/order.actions';
+import Link from 'next/link';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { formatCurrency, formatDateTime, formatId } from '@/lib/utils';
+import Pagination from '@/components/shared/pagination';
 
-const OrdersPage = () => {
-  return <>Orders: {ORDERS_PER_PAGE}</>;
+export const metadata: Metadata = {
+  title: 'My Orders',
+};
+
+const OrdersPage = async (props: {
+  searchParams: Promise<{ page: string }>;
+}) => {
+  const { page } = await props.searchParams;
+
+  const orders = await getMyOrders({
+    limit: ORDERS_PER_PAGE,
+    page: Number(page) || 1,
+  });
+
+  return (
+    <div className="space-y-2">
+      <h2 className="h2-bold">Orders</h2>
+      <div className="overflow-x-auto">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Order ID</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead>Total</TableHead>
+              <TableHead>Paid</TableHead>
+              <TableHead>Delived</TableHead>
+              <TableHead>Details</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {orders.data.map((order) => (
+              <TableRow key={order.id}>
+                <TableCell>{formatId(order.id)}</TableCell>
+                <TableCell>
+                  {formatDateTime(order.createdAt).dateTime}
+                </TableCell>
+                <TableCell>{formatCurrency(order.totalPrice)}</TableCell>
+                <TableCell>
+                  {order.isPaid && order.paidAt
+                    ? formatDateTime(order.paidAt).dateTime
+                    : 'No'}
+                </TableCell>
+                <TableCell>
+                  {order.isDelivered && order.deliveredAt
+                    ? formatDateTime(order.deliveredAt).dateTime
+                    : 'No'}
+                </TableCell>
+                <TableCell>
+                  <Link href={`/order/${order.id}`}>
+                    <span className="px-2">Details</span>
+                  </Link>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        {orders.totalPages > 1 && (
+          <Pagination
+            page={Number(page) || 1}
+            totalPages={orders?.totalPages}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default OrdersPage;
