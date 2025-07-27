7627ddbf2daf0258101209f11faca4fb2d958f07
andbudha <andreibartov@gmail.com>
2025-07-27 10:05:20 +0200

    Admin button added to the user button

--- components/shared/header/user-button.tsx
+++ components/shared/header/user-button.tsx
@@ -57,7 +57,14 @@ const UserButton = async () => {
                   Order History
                 </Link>
               </DropdownMenuItem>
-              <DropdownMenuItem className="p-0 mb-1">
+              {session?.user?.role === 'admin' && (
+                <DropdownMenuItem>
+                  <Link href={'/admin/overview'} className="w-full">
+                    Admin
+                  </Link>
+                </DropdownMenuItem>
+              )}
+              <DropdownMenuItem className="p-0 my-1">
                 <form action={signOutUser} className="w-full">
                   <Button
                     className="border py-4 px-2 h-4 w-full"
