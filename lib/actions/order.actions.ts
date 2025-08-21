186cf195a9f7708b6381a5b3d32def900f258c4e
andbudha <andreibartov@gmail.com>
2025-08-21 11:00:18 +0200

    both updateOrderToPaidCOD() and updateOrderToDelivered() actions created

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,5 +1,4 @@
 'use server';
-
 import { isRedirectError } from 'next/dist/client/components/redirect-error';
 import { convertToPlainObject, formatError } from '../utils';
 import { auth } from '@/auth';
@@ -388,6 +387,57 @@ export async function deleteOrder(orderId: string) {
     return {
       success: false,
       message: formatError(error) || 'An error occurred while deleting order!',
+    };
+  }
+}
+
+//update COD order to paid
+export async function updateOrderToPaidCOD(orderId: string) {
+  try {
+    await updateOrderToPaid({
+      orderId,
+    });
+    revalidatePath(`/order/${orderId}`);
+    return {
+      success: true,
+      message: 'Order marked as paid!!',
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: formatError(error) || 'An error occurred while updating order!',
+    };
+  }
+}
+
+//update COD order to delivered
+export async function updateOrderToDelivered(orderId: string) {
+  try {
+    const order = await prisma.order.findFirst({
+      where: {
+        id: orderId,
+      },
+    });
+    if (!order) throw new Error('Order not found');
+    if (!order.isPaid) throw new Error('Order is not paid');
+    await prisma.order.update({
+      where: {
+        id: orderId,
+      },
+      data: {
+        isDelivered: true,
+        deliveredAt: new Date(),
+      },
+    });
+    revalidatePath(`/order/${orderId}`);
+    return {
+      success: true,
+      message: 'Order marked as delivered!!',
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: formatError(error),
     };
   }
 }
