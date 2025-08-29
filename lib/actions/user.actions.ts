7c5f730e56bc05b3f2f50a56f9ff802c378a6a2f
andbudha <andreibartov@gmail.com>
2025-08-29 11:09:17 +0200

    updateUser() action created

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import {
   shippingAddressSchema,
   signInFormSchema,
   signUpFormSchema,
+  updateUserSchema,
 } from '../validators';
 import { auth, signIn, signOut } from '@/auth';
 import { isRedirectError } from 'next/dist/client/components/redirect-error';
@@ -215,6 +216,29 @@ export async function deleteUser(userId: string) {
     return {
       success: true,
       message: 'User deleted successfully!',
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: formatError(error),
+    };
+  }
+}
+
+//update user
+export async function updateUser(user: z.infer<typeof updateUserSchema>) {
+  try {
+    await prisma.user.update({
+      where: { id: user.id },
+      data: {
+        name: user.name,
+        role: user.role,
+      },
+    });
+    revalidatePath('/admin/users');
+    return {
+      success: true,
+      message: 'User updated successfully!',
     };
   } catch (error) {
     return {
