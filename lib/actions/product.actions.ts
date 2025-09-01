f47261df564590c8fc4b1c19b91a76df8365d4b7
andbudha <andreibartov@gmail.com>
2025-09-01 11:26:32 +0200

    getAllCategories action updated

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -155,16 +155,16 @@ export async function updateProduct(data: z.infer<typeof updateProductSchema>) {
 
 //get all categories
 export async function getAllCategories() {
-  try {
-    const data = await prisma.product.groupBy({
-      by: ['category'],
-      _count: true,
-    });
-    return data;
-  } catch (error) {
-    return {
-      success: false,
-      message: formatError(error),
-    };
-  }
+  const data = await prisma.product.groupBy({
+    by: ['category'],
+    _count: {
+      _all: true,
+    },
+  });
+  const categoriesWithCount = data.map((item) => ({
+    category: item.category,
+    count: item._count._all,
+  }));
+
+  return categoriesWithCount;
 }
