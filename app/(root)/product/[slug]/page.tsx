eb24cf435782ee0f9c18a652e26cdf6b370a6bec
andbudha <andreibartov@gmail.com>
2025-09-08 20:29:55 +0200

    Rating displayed in both ProductDetailsPage&ProductCard components

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -8,6 +8,7 @@ import { getMyCart } from '@/lib/actions/cart.actions';
 import { getProductBySlug } from '@/lib/actions/product.actions';
 import { notFound } from 'next/navigation';
 import ReviewList from './review-list';
+import Rating from '@/components/shared/product/rating';
 
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
@@ -35,9 +36,8 @@ const ProductDetailsPage = async (props: {
                 {product.brand}-{product.category}
               </p>
               <h1 className="h3-bold">{product.name}</h1>
-              <p>
-                {product.rating} Stars of {product.numReviews} Reviews
-              </p>
+              <Rating value={Number(product.rating)} />
+              <p>{product.numReviews} Reviews</p>
               <div className="flex flex-col sm:flex-row sm:items-center">
                 <ProductPrice
                   value={Number(product.price)}
