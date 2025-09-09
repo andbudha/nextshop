fb32309f2213cb2c285ecd991c6c5d8be1520aec
andbudha <andreibartov@gmail.com>
2025-09-09 13:14:33 +0200

    stripe-client-secret created

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -4,19 +4,40 @@ import { Metadata } from 'next';
 import OrderDetailsTable from './order-details-table';
 import { ShippingAddress } from '@/types';
 import { auth } from '@/auth';
+import Stripe from 'stripe';
 
 export const metadata: Metadata = {
   title: 'Order Details',
 };
-const OrderPage = async (props: { params: Promise<{ id: string }> }) => {
+const OrderDetailsPage = async (props: { params: Promise<{ id: string }> }) => {
   const { id } = await props.params;
 
   const order = await getOrderById(id);
   if (!order) notFound();
 
   const session = await auth();
+
+  let client_secret = null;
+
+  //check if is not paid and using stripe
+  if (order.paymentMethod === 'Stripe' && !order.isPaid) {
+    //initialize stripe instance
+    const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY!);
+    //create payment intent
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(Number(order.totalPrice) * 100),
+      currency: 'USD',
+      metadata: {
+        orderId: order.id,
+      },
+    });
+
+    client_secret = paymentIntent.client_secret;
+  }
+
   return (
     <OrderDetailsTable
+      stripeClientSecret={client_secret}
       order={{
         ...order,
         shippingAddress: order.shippingAddress as ShippingAddress,
@@ -27,4 +48,4 @@ const OrderPage = async (props: { params: Promise<{ id: string }> }) => {
   );
 };
 
-export default OrderPage;
+export default OrderDetailsPage;
