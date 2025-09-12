129a2406d755539908936303eb21d64d0ae65dd9
andbudha <andreibartov@gmail.com>
2025-09-12 11:28:44 +0200

    email index config file updated again

--- email/index.tsx
+++ email/index.tsx
@@ -1,17 +1,17 @@
 import { Resend } from 'resend';
-import { APP_NAME, EMAIL_SENDER } from '@/lib/constants';
+import { EMAIL_SENDER, APP_NAME } from '@/lib/constants';
 import { Order } from '@/types';
 import dotenv from 'dotenv';
-import PurchaseReceit from './purchase-receit';
 dotenv.config();
+import PurchaseReceit from './purchase-receit';
 
 const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY as string);
 
-export const sendPurchaseReceipt = async ({ order }: { order: Order }) => {
+export const sendPurchaseReceit = async ({ order }: { order: Order }) => {
   await resend.emails.send({
     from: `${APP_NAME} <${EMAIL_SENDER}>`,
     to: order.user.email,
-    subject: `Order Confirmation ${order.id}`,
+    subject: `Your order has been placed: ${order.id}`,
     react: <PurchaseReceit order={order} />,
   });
 };
